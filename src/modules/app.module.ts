892f6a4c3fa15e7ff042f560d1e30501215a2809
vanko <ivan.tym4ak@gmail.com>
2024-03-01 01:11:46 +0200

    added repositories

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -4,6 +4,7 @@ import { ConfigModule } from '@nestjs/config';
 import configuration from '../configs/configuration';
 import { HealthModule } from './health/health.module';
 import { PostgresModule } from './postgres/postgres.module';
+import { RepositoryModule } from './repository/repository.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
@@ -12,9 +13,10 @@ import { UsersModule } from './users/users.module';
       load: [configuration],
       isGlobal: true,
     }),
-    UsersModule,
     HealthModule,
     PostgresModule,
+    RepositoryModule,
+    UsersModule,
   ],
   controllers: [],
   providers: [],
